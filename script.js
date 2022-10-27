a8280890fee226e730feb7a504a968a747cc195e
maria pigazzi <maria.pigazzi98@gmail.com>
2022-10-27 19:51:01 +0200

    first draft

---
+++ script.js
@@ -0,0 +1,11 @@
+// add function
+
+// subtract function
+
+// multiply function
+
+// divide function
+
+// function operate - takes operator and two numbers and then calls one of the above functions on the numbers
+
+//
